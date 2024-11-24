5d9d7d8fff6d6b838b54c50ead86b2eccaa29720
Alexandre Sousa Silva <alexandresousasilva@icloud.com>
2024-11-24 13:55:39 -0300

    feat: add product page as response to the product route

---
+++ node-farm/scripts/products.js
@@ -0,0 +1,12 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Variables equivalents to __dirname and __filename in CommonJS
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const dataPath = path.join(__dirname, '../data');
+
+const productsText = fs.readFileSync(`${dataPath}/products.json`, 'utf-8');
+const getProducts = () => JSON.parse(productsText);
+export default getProducts;
--- node-farm/server.js
+++ node-farm/server.js
@@ -1,65 +1,37 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import http from 'http';
-import fs from 'fs';
+import url from 'url';
+import getProducts from './scripts/products.js';
+import { fillTemplate, templates } from './scripts/templates.js';
 
-// Variables equivalents to __dirname and __filename in CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Templates
-const overviewTemplate = fs.readFileSync(
-  `${__dirname}/templates/template-overview.html`,
-  'utf-8'
-);
-const cardTemplate = fs.readFileSync(
-  `${__dirname}/templates/template-card.html`,
-  'utf-8'
-);
-const productTemplate = fs.readFileSync(
-  `${__dirname}/templates/template-product.html`,
-  'utf-8'
-);
-
-// Products data
-const productsText = fs.readFileSync(
-  `${__dirname}/dev-data/data.json`,
-  'utf-8'
-);
-const products = JSON.parse(productsText);
-
-// Auxiliary functions
-const replaceTemplate = (temp, product) => {
-  let output = temp.replaceAll('{%PRODUCTNAME%}', product.productName);
-  output = output.replaceAll('{%IMAGE%}', product.image);
-  output = output.replaceAll('{%PRICE%}', product.price);
-  output = output.replaceAll('{%FROM%}', product.from);
-  output = output.replaceAll('{%NUTRIENTS%}', product.nutrients);
-  output = output.replaceAll('{%QUANTITY%}', product.quantity);
-  output = output.replaceAll('{%DESCRIPTION%}', product.description);
-  output = output.replaceAll('{%ID%}', product.id);
-  return output;
-};
+const products = getProducts();
 
 // Server
 const server = http.createServer((req, res) => {
-  const route = req.url;
+  const { pathname, query } = url.parse(req.url, true);
 
-  switch (route) {
+  switch (pathname) {
     // Overview Route (main route)
     case '/':
     case '/overview':
       res.writeHead(200, {
         'Content-type': 'text/html',
       });
-      const cardsHtml = products.map((el) => replaceTemplate(cardTemplate, el));
-      const output = overviewTemplate.replace('{%PRODUCT_CARDS%}', cardsHtml);
-      res.end(output);
+      const cardsHtml = products.map((el) => fillTemplate(templates.card, el));
+      const ovewviewPage = templates.overview.replace(
+        '{%PRODUCT_CARDS%}',
+        cardsHtml
+      );
+      res.end(ovewviewPage);
       break;
 
     // Product Route
     case '/product':
-      res.end('This is the product');
+      res.writeHead(200, {
+        'Content-type': 'text/html',
+      });
+      const product = products.find((el) => el.id === parseInt(query.id));
+      const productPage = fillTemplate(templates.product, product);
+      res.end(productPage);
       break;
 
     // API
@@ -67,7 +39,7 @@ const server = http.createServer((req, res) => {
       res.writeHead(200, {
         'Content-type': 'application/json',
       });
-      res.end(productsText);
+      res.end(JSON.stringify(products));
       break;
 
     // 404
