ef4f0b029f8f99f703adc2297e781c98f73e2bb7
Alexandre Sousa Silva <alexandresousasilva@icloud.com>
2024-11-27 01:48:17 -0300

    feat(natours): add get tours endpoint

--- natours/app.ts
+++ natours/app.ts
@@ -1,10 +1,22 @@
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
 
+const __dirname = import.meta.dirname;
+const toursDataPath = path.join(__dirname, 'dev-data', 'data', 'tours.json');
+const tours = JSON.parse(fs.readFileSync(toursDataPath, 'utf-8'));
+
 // Routes
-app.get('/', (req, res) => {
-  res.status(200).send('Hello from the server side!');
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).send({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
