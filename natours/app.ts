039985a03e4035ebde3501432e090ebfefc4c58d
Alexandre Sousa Silva <alexandresousasilva@icloud.com>
2024-11-27 22:22:04 -0300

    feat(natours): add post tour endpoint

--- natours/app.ts
+++ natours/app.ts
@@ -2,11 +2,23 @@ import express from 'express';
 import fs from 'fs';
 import path from 'path';
 
-const app = express();
-
 const __dirname = import.meta.dirname;
-const toursDataPath = path.join(__dirname, 'dev-data', 'data', 'tours.json');
+const toursDataPath = path.join(
+  __dirname,
+  'dev-data',
+  'data',
+  'tours-simple.json'
+);
 const tours = JSON.parse(fs.readFileSync(toursDataPath, 'utf-8'));
+
+// Server
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
 
 // Routes
 app.get('/api/v1/tours', (req, res) => {
@@ -19,7 +31,25 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+app.post('/api/v1/tours', (req, res) => {
+  const { _id, ...tour } = req.body;
+  const newId = tours.at(-1).id ?? 0 + 1;
+  const newTour = { id: newId, ...tour };
+  tours.push(newTour);
+
+  fs.writeFile(toursDataPath, JSON.stringify(tours), 'utf-8', (err) => {
+    if (err) {
+      res.status(500).send({
+        status: 'error',
+        message: 'Could not add tour',
+      });
+    } else {
+      res.status(201).send({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  });
 });
