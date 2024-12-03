3e8af4a04a7a3c8bc0d7a773e9421e4f70332162
Alexandre Sousa Silva <alexandresousasilva@icloud.com>
2024-12-02 23:40:28 -0300

    feat: add .env file and setup initial variables

--- natours/src/app.ts
+++ natours/src/app.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import morgan from 'morgan';
+import path from 'path';
 import tourRouter from './routes/tourRoutes.ts';
 import userRouter from './routes/userRoutes.ts';
 
 const app = express();
+
+// Middlewares
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(express.static(path.join(import.meta.dirname, '../public')));
 
 // Routes
 app.use('/api/v1/tours', tourRouter);
